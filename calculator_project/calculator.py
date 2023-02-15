0861d8378c494d96dbe465a441cd4954703a2df3
alvis254 <ngangaalvis@gmail.com>
2023-02-15 11:50:24 -0800

    added calculation operations

--- calculator_project/calculator.py
+++ calculator_project/calculator.py
@@ -9,16 +9,33 @@ operator = input("enter operator: ")
 
 def add(num1,num2):
     result = num1+num2
-    return result
+    print(result)
 
- def subtract(num1,num2):
+def subtract(num1,num2):
     result = num1-num2
-    return result
+    print(result)
 
- def divide(num1,num2):
+def divide(num1,num2):
     result = num1/num2
-    return result
+    print(result)
 
-  def multiply(num1,num2):
+def multiply(num1,num2):
     result = num1*num2
-    return result 
+    print(result)
+
+if operator == "+":
+    add(number1, number2)
+
+elif operator == "-":
+    subtract(number1, number2)
+
+elif operator == "/":
+    divide(number1, number2)
+
+elif operator == "x" or operator == "x":
+    multiply(number1, number2)
+    print(result)
+
+else:
+    print("invalid operator")
+    
